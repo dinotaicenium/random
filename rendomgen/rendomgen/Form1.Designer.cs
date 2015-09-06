d2e62e7296d6a4b0fbb2382b88887a212ac1d88e
yasitha <yasithdinesh@gmail.com>
2015-09-06 22:02:58 +0530

    changes

---
+++ rendomgen/rendomgen/Form1.Designer.cs
@@ -0,0 +1,133 @@
+﻿namespace rendomgen
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtFrom = new System.Windows.Forms.TextBox();
+            this.txtTo = new System.Windows.Forms.TextBox();
+            this.cmdReset = new System.Windows.Forms.Button();
+            this.lblNo = new System.Windows.Forms.Label();
+            this.cmdGenerate = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtFrom
+            // 
+            this.txtFrom.Location = new System.Drawing.Point(38, 32);
+            this.txtFrom.Name = "txtFrom";
+            this.txtFrom.Size = new System.Drawing.Size(100, 20);
+            this.txtFrom.TabIndex = 0;
+            // 
+            // txtTo
+            // 
+            this.txtTo.Location = new System.Drawing.Point(165, 32);
+            this.txtTo.Name = "txtTo";
+            this.txtTo.Size = new System.Drawing.Size(100, 20);
+            this.txtTo.TabIndex = 1;
+            // 
+            // cmdReset
+            // 
+            this.cmdReset.Location = new System.Drawing.Point(190, 58);
+            this.cmdReset.Name = "cmdReset";
+            this.cmdReset.Size = new System.Drawing.Size(75, 23);
+            this.cmdReset.TabIndex = 2;
+            this.cmdReset.Text = "Reset";
+            this.cmdReset.UseVisualStyleBackColor = true;
+            this.cmdReset.Click += new System.EventHandler(this.cmdReset_Click);
+            // 
+            // lblNo
+            // 
+            this.lblNo.AutoSize = true;
+            this.lblNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 21.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNo.Location = new System.Drawing.Point(71, 114);
+            this.lblNo.Name = "lblNo";
+            this.lblNo.Size = new System.Drawing.Size(157, 33);
+            this.lblNo.TabIndex = 3;
+            this.lblNo.Text = "Random #";
+            // 
+            // cmdGenerate
+            // 
+            this.cmdGenerate.Location = new System.Drawing.Point(98, 161);
+            this.cmdGenerate.Name = "cmdGenerate";
+            this.cmdGenerate.Size = new System.Drawing.Size(75, 23);
+            this.cmdGenerate.TabIndex = 4;
+            this.cmdGenerate.Text = "Generate";
+            this.cmdGenerate.UseVisualStyleBackColor = true;
+            this.cmdGenerate.Click += new System.EventHandler(this.cmdGenerate_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(30, 13);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "From";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(165, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(20, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "To";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cmdGenerate);
+            this.Controls.Add(this.lblNo);
+            this.Controls.Add(this.cmdReset);
+            this.Controls.Add(this.txtTo);
+            this.Controls.Add(this.txtFrom);
+            this.Name = "Form1";
+            this.Text = "Random #";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtFrom;
+        private System.Windows.Forms.TextBox txtTo;
+        private System.Windows.Forms.Button cmdReset;
+        private System.Windows.Forms.Label lblNo;
+        private System.Windows.Forms.Button cmdGenerate;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ rendomgen/rendomgen/Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+
+namespace rendomgen
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void cmdReset_Click(object sender, EventArgs e)
+        {
+
+            if(txtFrom.Text=="" || txtTo.Text=="")
+            {
+                MessageBox.Show("please fill from and to");
+                return;
+            }
+            OleDbConnection oleDbConnection = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\random\\db.accdb;Persist Security Info=False;");
+            
+            oleDbConnection.Open();
+
+            OleDbCommand cmd1 = new OleDbCommand();
+
+            cmd1.Connection = oleDbConnection;
+
+            cmd1.CommandText = "DELETE from series";
+
+            cmd1.ExecuteNonQuery();
+
+            for (int i =Convert.ToInt32(txtFrom.Text); i <= Convert.ToInt32(txtTo.Text); i++)
+            {
+                OleDbCommand cmd = new OleDbCommand();
+
+                cmd.Connection = oleDbConnection;
+
+                cmd.CommandText = "INSERT INTO series([Number]) VALUES (@No)";
+
+                cmd.Parameters.Add("@No", OleDbType.Numeric).Value = i;
+
+                cmd.ExecuteNonQuery();
+            }
+
+
+            oleDbConnection.Close();
+        }
+
+        private void cmdGenerate_Click(object sender, EventArgs e)
+        {
+            string connectionString = @"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=C:\\random\\db.accdb;Persist Security Info=False;";
+
+            string queryString = "SELECT number FROM [series] where given=0 order by number";
+            try
+            {
+                using (OleDbConnection connection = new OleDbConnection(connectionString))
+                {
+                    OleDbCommand command = new OleDbCommand(queryString, connection);
+                    connection.Open();
+
+                    OleDbCommand cmd = new OleDbCommand("select count(*) FROM [series] where given=0", connection);
+
+                    int total = (Int32)cmd.ExecuteScalar();
+  
+
+                    OleDbDataReader reader = command.ExecuteReader();
+
+             
+                    Random r = new Random();
+                    int rInt = r.Next(1, total); //for ints
+                    int i = 1;
+                    while (reader.Read())
+                    {
+                        Int32 randomno = Convert.ToInt32(reader.GetValue(0));
+                       // lblNo.Text = randomno.ToString();
+                        if (i == rInt)
+                        {
+                            lblNo.Text = randomno.ToString();
+                            cmd.CommandText = "Update series set given=1 where [Number]=@No";
+
+                            cmd.Parameters.Add("@No", OleDbType.Numeric).Value = randomno;
+
+                            cmd.ExecuteNonQuery();
+                            break;
+                        }
+                        i++;
+                    }
+                    reader.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Failed to connect to data source");
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
